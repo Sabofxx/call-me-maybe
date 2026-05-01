d7acd154720c31d5b4b151b28e3b6bf642ee8f23
Oscar Mischler <Sabo.fx@proton.me>
2026-05-01 16:24:42 +0200

    fix-cmm

--- src/constrained_dec.py
+++ src/constrained_dec.py
@@ -7,7 +7,7 @@ from typing import TYPE_CHECKING, TypedDict
 from src.models import FunctionDefinition
 
 if TYPE_CHECKING:  # pragma: no cover - import is only used for type hints
-    from llm_sdk.llm_sdk import Small_LLM_Model
+    from llm_sdk import Small_LLM_Model
 
 
 class TrieNode(TypedDict):
